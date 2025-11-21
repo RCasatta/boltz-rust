72322c704558c58a4f6bc63f491aacd30c896203
Riccardo Casatta <riccardo@casatta.it>
2025-11-21 15:29:20 +0100

    wip

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -37,8 +37,15 @@ pub async fn sleep(duration: Duration) {
     }
     #[cfg(all(target_family = "wasm", target_os = "unknown"))]
     {
-        let timeout_ms = duration.as_millis() as u32;
-        TimeoutFuture::new(timeout_ms).await;
+        let millis = duration.as_millis() as i32;
+        let mut cb = |resolve: js_sys::Function, _reject: js_sys::Function| {
+            web_sys::window()
+                .unwrap()
+                .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, millis)
+                .unwrap();
+        };
+        let p = js_sys::Promise::new(&mut cb);
+        wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
     }
 }
 
